9b6ce1b523eebd37625b86d9542eba67ad7beea6
Cajulis Manry, Amber <amber.manry@icfi.com>
2015-09-07 20:54:43 -0400

    changed hungr.js to hungr.jsx

---
+++ hungr.jsx
@@ -0,0 +1,11 @@
+// Define a collection to hold our suggestions
+Suggestions = new Mongo.Collection("suggestions");
+
+if (Meteor.isClient) {
+    // This code is executed on the client only
+
+    Meteor.startup(function () {
+        // Use Meteor.startup to render the component after the page is ready
+        React.render(<App />, document.getElementById("render-target"));
+    });
+}
