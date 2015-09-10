2168627ba5bb9c4629a58688d0f3d88e246281cf
Cajulis Manry, Amber <amber.manry@icfi.com>
2015-09-09 22:27:33 -0400

    created dropdowns for additional fields

--- App.jsx
+++ App.jsx
@@ -23,8 +23,8 @@ App = React.createClass({
         // Find the text field via the React ref
         var place = React.findDOMNode(this.refs.place).value.trim();
         var startTime = React.findDOMNode(this.refs.startTime).value;
-        var endTime = React.findDOMNode(this.refs.endTime).value.trim();
-        var numAttending = React.findDOMNode(this.refs.numAttending).value.trim();
+        var endTime = React.findDOMNode(this.refs.endTime).value;
+        var numAttending = React.findDOMNode(this.refs.numAttending).value;
         var notes = React.findDOMNode(this.refs.notes).value.trim();
 
         Suggestions.insert({
@@ -88,16 +88,57 @@ App = React.createClass({
                     </select>
                   </div>
                   <div class="form-group">
-                  End Time<input
-                    type="text"
-                    ref="endTime"
-                    placeholder="" />
+                  <label for="startTime">End Time:</label>
+                      <select class="form-control" id="endTime" ref="endTime">
+                      <option>10:00 AM</option>
+                      <option>10:15 AM</option>
+                      <option>10:30 AM</option>
+                      <option>10:45 AM</option>
+                      <option>11:00 AM</option>
+                      <option>11:15 AM</option>
+                      <option>11:30 AM</option>
+                      <option>11:45 AM</option>
+                      <option>12:00 PM</option>
+                      <option>12:15 PM</option>
+                      <option>12:30 PM</option>
+                      <option>12:45 PM</option>
+                      <option>1:00 PM</option>
+                      <option>1:15 PM</option>
+                      <option>1:30 PM</option>
+                      <option>1:45 PM</option>
+                      <option>2:00 PM</option>
+                      <option>2:15 PM</option>
+                      <option>2:30 PM</option>
+                      <option>2:45 PM</option>
+                      <option>3:00 PM</option>
+                      <option>3:15 PM</option>
+                      <option>3:30 PM</option>
+                      <option>3:45 PM</option>
+                      <option>4:00 PM</option>
+                      <option>4:15 PM</option>
+                      <option>4:30 PM</option>
+                      <option>4:45 PM</option>
+                      </select>
                   </div>
                   <div class="form-group">
-                  Number Attending<input
-                      type="text"
-                      ref="numAttending"
-                      placeholder="" />
+                  <label for="numAttending">Number Attending:</label>
+                  <select class="form-control" id="numAttending" ref="numAttending">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    </select>
                   </div>
                   <div class="form-group">
                     Notes<input
