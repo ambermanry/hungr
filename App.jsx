10508e30c8740349dd0c35736d68c186485f41a4
Cajulis Manry, Amber <amber.manry@icfi.com>
2015-09-09 22:21:51 -0400

    added dropdown for start time

--- App.jsx
+++ App.jsx
@@ -22,7 +22,7 @@ App = React.createClass({
 
         // Find the text field via the React ref
         var place = React.findDOMNode(this.refs.place).value.trim();
-        var startTime = React.findDOMNode(this.refs.startTime).value.trim();
+        var startTime = React.findDOMNode(this.refs.startTime).value;
         var endTime = React.findDOMNode(this.refs.endTime).value.trim();
         var numAttending = React.findDOMNode(this.refs.numAttending).value.trim();
         var notes = React.findDOMNode(this.refs.notes).value.trim();
@@ -55,10 +55,37 @@ App = React.createClass({
                   <input type="text" ref="place" class="form-control" id="place"/>
                   </div>
                   <div class="form-group">
-                  Start Time<input
-                      type="text"
-                      ref="startTime"
-                      placeholder="" />
+                  <label for="startTime">Start Time:</label>
+                    <select class="form-control" id="startTime" ref="startTime">
+                    <option>10:00 AM</option>
+                    <option>10:15 AM</option>
+                    <option>10:30 AM</option>
+                    <option>10:45 AM</option>
+                    <option>11:00 AM</option>
+                    <option>11:15 AM</option>
+                    <option>11:30 AM</option>
+                    <option>11:45 AM</option>
+                    <option>12:00 PM</option>
+                    <option>12:15 PM</option>
+                    <option>12:30 PM</option>
+                    <option>12:45 PM</option>
+                    <option>1:00 PM</option>
+                    <option>1:15 PM</option>
+                    <option>1:30 PM</option>
+                    <option>1:45 PM</option>
+                    <option>2:00 PM</option>
+                    <option>2:15 PM</option>
+                    <option>2:30 PM</option>
+                    <option>2:45 PM</option>
+                    <option>3:00 PM</option>
+                    <option>3:15 PM</option>
+                    <option>3:30 PM</option>
+                    <option>3:45 PM</option>
+                    <option>4:00 PM</option>
+                    <option>4:15 PM</option>
+                    <option>4:30 PM</option>
+                    <option>4:45 PM</option>
+                    </select>
                   </div>
                   <div class="form-group">
                   End Time<input
